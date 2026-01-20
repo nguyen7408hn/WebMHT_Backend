50046a8d01cfa304b696446c6a9e668d7c140705
nguyen7408hn <45986020+nguyen7408hn@users.noreply.github.com>
2026-01-20 08:45:47 +0700

    Update WebConfig.java
    Update allowed origins in CORS configuration: lengocvan.netlify.app

--- src/main/java/com/mht/my_web/config/WebConfig.java
+++ src/main/java/com/mht/my_web/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://lmkn.netlify.app", "http://localhost:5173")
+                .allowedOrigins("https://lengocvan.netlify.app", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
