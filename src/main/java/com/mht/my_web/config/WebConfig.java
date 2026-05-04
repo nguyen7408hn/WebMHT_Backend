f0043f435d824e8ae6a62ea2545d9b7979e40beb
nguyen7408hn <45986020+nguyen7408hn@users.noreply.github.com>
2026-05-04 11:50:52 +0700

    update domain Netlify
    Doi ten mien theo Frontend Netlify

--- src/main/java/com/mht/my_web/config/WebConfig.java
+++ src/main/java/com/mht/my_web/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://lengocvan.netlify.app", "http://localhost:5173")
+                .allowedOrigins("https://echaydiu1210.netlify.app", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
